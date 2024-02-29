9386b66a78f1eeb427d99575888b616a7942b9f5
samba <gudurusamba@gmail.com>
2024-02-29 10:09:30 +0530

    vars

---
+++ learn_terraform/01 vars/vars.tf
@@ -0,0 +1,11 @@
+variable "sample_sring" {
+  default = "hello world"
+}
+
+output "sample_string" {
+  value = var.sample_sring
+}
+
+output "sample_string_duplicate" {
+  value = "value of sample string ${var.sample_sring}"
+}
